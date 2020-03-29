7dd1ba19d037f942f2a2e1d6416b31394691698e
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-29 14:22:09 -0300

    Handling Invalid Input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,7 +18,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input!");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
 
