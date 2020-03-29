c55fbb8e714a876500091e729e2e9a5bbf8d8fc1
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-29 14:17:32 -0300

    Allowing Multiple Guesses with Looping

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,7 +18,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }
 
         println!("You guessed: {}", guess);
 
