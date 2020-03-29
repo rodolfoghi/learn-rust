30e34344b1d0a6b8b6661b392c58c3e1d606feb6
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-29 14:08:50 -0300

    Guessing game Generating a Random Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    println!("The secrete number is {}", secret_number);
 
     println!("Please input your guess.");
 
