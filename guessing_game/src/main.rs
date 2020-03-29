40d3b6fd80365e30b7acc2d941251d50ca645194
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-03-29 14:18:28 -0300

    Revert "Allowing Multiple Guesses with Looping"
    This reverts commit c55fbb8e714a876500091e729e2e9a5bbf8d8fc1.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,10 +18,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        }
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
 
         println!("You guessed: {}", guess);
 
